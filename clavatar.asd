61c8171674940ac60554e5e4e5ff1dbf39029c71
Pixie <pix@kepibu.org>
2012-08-25 04:39:55 -0500

    Figure out what to use by 404s, rather than relying on services to redirect URLs properly

--- clavatar.asd
+++ clavatar.asd
@@ -2,7 +2,7 @@
   :description "Determines appropriate URLs for avatars using assorted avatar services."
   :author "Pixie <pix@kepibu.org>"
   :license "MIT"
-  :depends-on ("iolib" "ironclad" "babel" "do-urlencode")
+  :depends-on ("iolib" "ironclad" "babel" "do-urlencode" "drakma")
   :serial t
   :components ((:file "package")
                (:file "util")
--- clavatar.lisp
+++ clavatar.lisp
@@ -60,14 +60,15 @@
   "Returns an avatar URL for a given identifier (e-mail address).
 
 First, checks if the domain of identifier provides some clue as to how to get
-avatars.  If so, uses that.  Otherwise, tries SERVICES in order, finally
-defaulting to DEFAULT.
+avatars.  If so, uses that.  Otherwise, tries SERVICES in order and uses the
+first service which does not return a 404.  Will use DEFAULT on the last service
+if no service knows about the identifier.
 
-Note that this chaining behavior relies on the ability to specify an URL as the
-default behavior for services, so any service in the chain which does not
-support the default argument will break the chain."
+Note that because this involves DNS and HTTP queries, it is slow and you should
+cache the results."
   (or (avatar-url 'federated identifier :size size :default default)
-      (loop :for service :in (reverse services)
-            :for def := default :then url
-            :for url := (avatar-url service identifier :size size :default def)
-            :finally (return url))))
+      (loop :for service :in (butlast services)
+            :for url := (avatar-url service identifier :size size :default 404)
+            :unless (= 404 (nth-value 1 (drakma:http-request url :redirect nil)))
+              :do (return url))
+      (avatar-url (car (last services)) identifier :default default)))
